baec1a30ed684bba35e3dea02d521cfa93eecc83
darideveloper <darideveloper@gmail.com>
2022-12-20 19:11:48 -0600

    urls: admin at home path

--- ezbookingtours_store/urls.py
+++ ezbookingtours_store/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('store/', include('store.urls'))
 ]
