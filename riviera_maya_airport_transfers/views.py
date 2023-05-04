54c7429d0d6b4135ddacb95affca2731c0461873
darideveloper <darideveloper@gmail.com>
2023-05-04 11:36:22 -0600

    feat (riviera/views): allow cors

--- riviera_maya_airport_transfers/views.py
+++ riviera_maya_airport_transfers/views.py
@@ -1,6 +1,7 @@
 from . import models
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
 def index (request):
     """ Redirect to store admin page """
@@ -9,6 +10,7 @@ def index (request):
     }
     return JsonResponse(response)
 
+@csrf_exempt
 def airbnb_municipalities (request):
     
     data = models.AirbnbMunicipality.objects.all()
@@ -16,13 +18,15 @@ def airbnb_municipalities (request):
     
     return HttpResponse (data_serialized, content_type="application/json") 
 
+@csrf_exempt
 def hotels (request):
     
     data = models.Hotel.objects.all()
     data_serialized = serializers.serialize("json", data)
     
     return HttpResponse (data_serialized, content_type="application/json") 
-    
+
+@csrf_exempt
 def transports (request):
     
     data = models.Transport.objects.all()
