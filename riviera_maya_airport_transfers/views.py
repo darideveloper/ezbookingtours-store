355eca57013c9f6e6573cbb567e235c4c0cf285e
darideveloper <darideveloper@gmail.com>
2023-05-04 12:54:09 -0600

    feat (riviera/views): short data by name

--- riviera_maya_airport_transfers/views.py
+++ riviera_maya_airport_transfers/views.py
@@ -1,8 +1,27 @@
+import json
 from . import models
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 
+def __sort_data__ (data:list):
+    """ Soprt data serializable by name field
+
+    Args:
+        data (list): dictionaries with fields
+    """
+    
+    data = json.loads(data)
+        
+    data_sorted = []
+    names = list(map(lambda row: row["fields"]["name"], data))
+    names.sort()
+    for name in names:
+        current_row = list(filter(lambda row: row["fields"]["name"] == name, data))[0]
+        data_sorted.append (current_row)  
+        
+    return json.dumps(data_sorted)
+        
 def index (request):
     """ Redirect to store admin page """
     response = {
@@ -13,24 +32,36 @@ def index (request):
 @csrf_exempt
 def airbnb_municipalities (request):
     
+    # Get data
     data = models.AirbnbMunicipality.objects.all()
     data_serialized = serializers.serialize("json", data)
+    
+    # Order by name
+    data_serialized = __sort_data__ (data_serialized)
     
     return HttpResponse (data_serialized, content_type="application/json") 
 
 @csrf_exempt
 def hotels (request):
     
+    # Get data
     data = models.Hotel.objects.all()
     data_serialized = serializers.serialize("json", data)
     
+    # Order by name
+    data_serialized = __sort_data__ (data_serialized)
+        
     return HttpResponse (data_serialized, content_type="application/json") 
 
 @csrf_exempt
 def transports (request):
     
+    # Get data
     data = models.Transport.objects.all()
     data_serialized = serializers.serialize("json", data)
+    
+    # Order by name
+    data_serialized = __sort_data__ (data_serialized)
     
     return HttpResponse (data_serialized, content_type="application/json") 
     
