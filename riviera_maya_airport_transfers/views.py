b4005b15298175e04768faa282fcbdfba0b054b1
darideveloper <darideveloper@gmail.com>
2023-05-04 11:19:51 -0600

    feat (riviera/views): return models data

--- riviera_maya_airport_transfers/views.py
+++ riviera_maya_airport_transfers/views.py
@@ -1,4 +1,6 @@
+from . import models
 from django.http import HttpResponse, JsonResponse
+from django.core import serializers
 
 def index (request):
     """ Redirect to store admin page """
@@ -9,13 +11,22 @@ def index (request):
 
 def airbnb_municipalities (request):
     
-    return HttpResponse ("Hello world") 
+    data = models.AirbnbMunicipality.objects.all()
+    data_serialized = serializers.serialize("json", data)
+    
+    return HttpResponse (data_serialized, content_type="application/json") 
 
 def hotels (request):
     
-    return HttpResponse ("Hello world") 
+    data = models.Hotel.objects.all()
+    data_serialized = serializers.serialize("json", data)
+    
+    return HttpResponse (data_serialized, content_type="application/json") 
     
 def transports (request):
     
-    return HttpResponse ("Hello world") 
+    data = models.Transport.objects.all()
+    data_serialized = serializers.serialize("json", data)
+    
+    return HttpResponse (data_serialized, content_type="application/json") 
     
