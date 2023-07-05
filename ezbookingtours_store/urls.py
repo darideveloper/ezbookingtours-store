cd4e2ff1db5be448f65c0e212b675a616e7fdbe0
daridev <darideveloper@gmail.com>
2023-07-05 12:57:50 -0600

    feat (admin): update site title

--- ezbookingtours_store/urls.py
+++ ezbookingtours_store/urls.py
@@ -2,6 +2,11 @@ from . import views
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Omar Dashboard"
+admin.site.site_title = 'Omar Dashboard'
+admin.site.site_url = ''
+admin.site.index_title = "Admin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,5 @@
 from . import views
 from django.urls import path
-from django.contrib import admin
-
-admin.site.site_header = "Ezbookingtours Store"
-admin.site.site_title = 'Ezbookingtours Store'
-admin.site.site_url = 'https://ezbookingtours.com'
-admin.site.index_title = "Admin"
-
-app_name = "store"
 
 urlpatterns = [
     path ('', views.index, name="index"),
