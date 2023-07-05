5c43727ca6b322c179c8356700af55b69f5228d9
daridev <darideveloper@gmail.com>
2023-07-04 21:20:03 -0600

    feat (wedding/admin): register models

--- wedding/admin.py
+++ wedding/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from wedding import models
 
-# Register your models here.
+@admin.register (models.Sale)
+class SaleAdmin (admin.ModelAdmin):
+    list_display = ('name', 'last_name', 'sale_datetime', 'arriving_price', 'departure_price', 'vip_code', 'is_paid')
+    ordering = ('sale_datetime', 'name', 'last_name')
+    list_filter = ('sale_datetime', 'is_paid', 'vip_code')
+    list_max_show_all = 50
+    
+@admin.register (models.VipCode)
+class VipCodeAdmin (admin.ModelAdmin):
+    list_display = ('value', 'enabled')
+    ordering = ('value',)
+    list_filter = ('enabled',)
+    list_max_show_all = 50
+    
+@admin.register (models.Settings)
+class SettingsAdmin (admin.ModelAdmin): 
+    list_display = ('name', 'value')
+    ordering = ('name',)
+    list_max_show_all = 50
