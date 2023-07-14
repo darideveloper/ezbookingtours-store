5892eb31ade4319f6e2c62a3c9fe6f4f0f65c6fd
daridev <darideveloper@gmail.com>
2023-07-14 11:33:18 -0600

    feat (wedding/admin/Sale): order by -date

--- wedding/admin.py
+++ wedding/admin.py
@@ -4,7 +4,7 @@ from wedding import models
 @admin.register (models.Sale)
 class SaleAdmin (admin.ModelAdmin):
     list_display = ('name', 'last_name', 'sale_datetime', 'price', 'vip_code', 'is_paid', 'stripe_data')
-    ordering = ('sale_datetime', 'name', 'last_name')
+    ordering = ('-sale_datetime', 'name', 'last_name')
     list_filter = ('sale_datetime', 'is_paid', 'vip_code')
     search_fields = ('name', 'last_name', 'vip_code', 'stripe_data')
     list_max_show_all = 50
