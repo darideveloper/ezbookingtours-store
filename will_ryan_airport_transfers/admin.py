1c16b582f7e2a991f50f83b4459ae2ae033ea2a1
daridev <darideveloper@gmail.com>
2023-08-08 15:14:47 -0600

    feat (will_ryan/admin): register models

--- will_ryan_airport_transfers/admin.py
+++ will_ryan_airport_transfers/admin.py
@@ -1,3 +1,13 @@
+
+from . import models
 from django.contrib import admin
 
-# Register your models here.
+@admin.register (models.Hotel)
+class HotelAdmin (admin.ModelAdmin):
+    list_display = ('name', 'extra_price')
+    ordering = ('name',)
+    
+@admin.register (models.Transport)
+class TransportAdmin (admin.ModelAdmin):
+    list_display = ('key', 'name', 'price', 'por_defecto',)
+    ordering = ('name',)
