0d0c664de2e864fb9715704995553fb3d673564d
daridev <darideveloper@gmail.com>
2024-06-29 11:42:52 -0600

    feat (rohan_karisma): connect with stripe_api_flask

--- rohan_karisma/views.py
+++ rohan_karisma/views.py
@@ -1,9 +1,16 @@
+import os
 import json
-from django.views.decorators.csrf import csrf_exempt
+import requests
+from dotenv import load_dotenv
 from django.views import View
-from django.utils.decorators import method_decorator
-from django.http import JsonResponse
 from rohan_karisma import models
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+load_dotenv()
+STRIPE_FLASK_API = os.getenv("STRIPE_FLASK_API")
+HOST = os.getenv("HOST")
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -12,16 +19,14 @@ class SaleView(View):
     def post(self, request):
         
         # Get data
-        json_body = json.loads(request.body)
-        print(json_body)
-        print(json_body["products"].keys())
-        transport_type = list(json_body["products"].keys())[0]
+        json_data = json.loads(request.body)
+        transport_type = list(json_data["products"].keys())[0]
         
         # Get product data
-        product = json_body["products"][transport_type]
+        product = json_data["products"][transport_type]
         price = product["price"]
         
-        # Get details data
+        # Get description data
         description = product["description"]
         name = description["name"]
         last_name = description["last_name"]
@@ -32,13 +37,13 @@ class SaleView(View):
         # Get arriving and departing data
         arriving = ""
         departing = ""
-        for details_key, details_value in description.items():
-            if "arriving" in details_key:
-                key_clean = details_key.replace("arriving ", "")
-                arriving += f"{key_clean}: {details_value}\n"
-            elif "departing" in details_key:
-                key_clean = details_key.replace("departing ", "")
-                departing += f"{key_clean}: {details_value}\n"
+        for description_key, description_value in description.items():
+            if "arriving" in description_key:
+                key_clean = description_key.replace("arriving ", "")
+                arriving += f"{key_clean}: {description_value} |\n"
+            elif "departing" in description_key:
+                key_clean = description_key.replace("departing ", "")
+                departing += f"{key_clean}: {description_value} |\n"
                 
         # Save model
         sale = models.Sale.objects.create(
@@ -48,12 +53,23 @@ class SaleView(View):
             email=email,
             passengers=passengers,
             price=price,
-            # arriving=arriving,
+            arriving=arriving,
             departing=departing,
             transport_vehicule=transport_vehicle,
         )
         
-        return JsonResponse({
-            "status": "success",
-            "message": f"Sale saved {json_body}"
-        })
+        # Create stripe link sending data to api
+        json_data["url_success"] = f'{HOST}/rohan-karisma/sale/{sale.id}'
+        description_text = ""
+        for description_key, description_value in description.items():
+            description_text += f"{description_key}: {description_value} | "
+        json_data["products"][transport_type]["description"] = description_text
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        res = requests.post(STRIPE_FLASK_API, json=json_data, headers=headers)
+        json_res = res.json()
+        
+        # Return same api response
+        return JsonResponse(json_res)
