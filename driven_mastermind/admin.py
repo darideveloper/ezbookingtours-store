9e14b003907ae02105ddb7a1f389695f4110984e
daridev <darideveloper@gmail.com>
2024-08-05 12:41:07 -0600

    feat (driven_mastermind): create Sale model

--- driven_mastermind/admin.py
+++ driven_mastermind/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from driven_mastermind import models
 
-# Register your models here.
+
+@admin.register(models.Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'client_email',
+        'client_first_name',
+        'client_last_name',
+        'passenger_number',
+        'hotel',
+        'arriving_date',
+        'arriving_time',
+        'arriving_airline',
+        'arriving_flight',
+        'departing_date',
+        'departing_time',
+        'departing_airline',
+        'departing_flight',
+        'reservation_date_time',
+        'total_price',
+        'is_paid',
+    )
+    list_display_links = (
+        'id',
+        'client_email',
+    )
+    list_filter = (
+        'is_paid',
+        'arriving_date',
+        'departing_date',
+        'reservation_date_time',
+    )
+    search_fields = (
+        'client_first_name',
+        'client_last_name',
+        'client_eamil',
+        'hotel',
+        'arriving_airline',
+        'arriving_flight',
+        'departing_airline',
+        'departing_flight',
+    )
--- driven_mastermind/models.py
+++ driven_mastermind/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Sale(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='ID')
+    client_email = models.EmailField(verbose_name='Correo del cliente')
+    client_first_name = models.CharField(verbose_name='Nombre del cliente')
+    client_last_name = models.CharField(verbose_name='Apellido del cliente')
+    passenger_number = models.IntegerField(verbose_name='Número de pasajeros')
+    hotel = models.CharField(verbose_name='Hotel')
+    arriving_date = models.DateField(verbose_name='Fecha de llegada')
+    arriving_time = models.TimeField(verbose_name='Hora de llegada')
+    arriving_airline = models.CharField(verbose_name='Aerolínea de llegada')
+    arriving_flight = models.CharField(verbose_name='Vuelo de llegada')
+    departing_date = models.DateField(verbose_name='Fecha de salida')
+    departing_time = models.TimeField(verbose_name='Hora de salida')
+    departing_airline = models.CharField(verbose_name='Aerolínea de salida')
+    departing_flight = models.CharField(verbose_name='Vuelo de salida')
+    reservation_date_time = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Fecha y hora de la reservación'
+    )
+    total_price = models.FloatField(verbose_name='Precio total')
+    is_paid = models.BooleanField(verbose_name='Está pagado', default=False)
