3d66e1477e32c4c3389e6699a671f24aa6c905f2
daridev <darideveloper@gmail.com>
2025-01-22 15:36:33 -0600

    feat (tony-thoa/admin): register all models

--- tony_thoa_airport_transfers/admin.py
+++ tony_thoa_airport_transfers/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from tony_thoa_airport_transfers import models
 
-# Register your models here.
+
+@admin.register(models.Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "last_name",
+        "sale_datetime",
+        "price",
+        "vip_code",
+        "is_paid",
+        "phone",
+        "email",
+        "stripe_data",
+    )
+    ordering = ("-sale_datetime", "name", "last_name")
+    list_filter = ("sale_datetime", "is_paid", "vip_code")
+    search_fields = ("name", "last_name", "vip_code", "stripe_data", "phone", "email")
+    list_max_show_all = 50
+
+
+@admin.register(models.VipCode)
+class VipCodeAdmin(admin.ModelAdmin):
+    list_display = ("value", "enabled")
+    ordering = ("value",)
+    list_filter = ("enabled",)
+    list_max_show_all = 50
+
+
+@admin.register(models.Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ("name", "extra_price")
+    ordering = ("name", "extra_price")
+
+
+@admin.register(models.Transport)
+class TransportAdmin(admin.ModelAdmin):
+    list_display = ("key", "name", "price", "by_default")
+    ordering = ("key", "name", "price", "by_default")
+
+
+@admin.register(models.FreeDays)
+class FreeDaysAdmin(admin.ModelAdmin):
+    list_display = ("date", "category")
+    ordering = ("date", "category")
+
+
+@admin.register(models.FreeDaysCategory)
+class FreeDaysCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    ordering = ("name",)
