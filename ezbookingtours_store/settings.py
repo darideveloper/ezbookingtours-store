2d51cf3a4e420469b956efd2ec42007364ed8b46
daridev <darideveloper@gmail.com>
2026-02-07 12:33:07 -0600

    fix (settings): default email credentials

--- ezbookingtours_store/settings.py
+++ ezbookingtours_store/settings.py
@@ -163,8 +163,8 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True"
 EMAIL_CLIENT = os.getenv("EMAIL_CLIENT")
 
 CORS_ALLOW_ALL_ORIGINS = True
